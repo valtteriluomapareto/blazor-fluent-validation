fb362bd0cbb8c0f855a16b6400fafb58dc18ac2e
Valtteri Luoma <valtteri.luoma@paretosoftware.fi>
2026-01-27 11:04:19 +0200

    Fix tailwind building

--- src/App.Ui/tailwind.config.js
+++ src/App.Ui/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{razor,html}"],
+    content: [
+        "./**/*.{razor,html,cs}",
+        "../App.Ui.Client/**/*.{razor,html,cs}",
+        "../App.Host/**/*.{razor,html,cs}",
+    ],
     theme: {
         extend: {},
     },
